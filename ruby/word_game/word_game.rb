41e0c4252f4e5a9aa12938ddd345a22a93ff0b65
Michael Mercuro <mercurom1@montclair.edu>
2017-01-22 14:29:59 -0500

    Add new method and test

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -2,6 +2,9 @@
 #user one inputs a word
 #user two trys to guess the word
 #the amount of guesses is limited to the length of the word
+# guess- loop is equal to word.length
+# need a condition in there as well to match the input letter with
+# with the letters in the word.  
 #repeated guess do not count
 #if a correct letter is guessed then it appears in its correct position
 #within the string to the user
@@ -16,5 +19,27 @@ class WordGame
 	def new_word
 		@word
 	end
-	
+
+	def letter_guess(letter)
+		new_array = []
+		word_array = @word.split("")
+		word_array.map! do |word_letter|
+			if word_letter != letter 
+				word_letter = "_"
+				new_array << word_letter
+			elsif word_letter === letter
+				word_letter = letter
+				new_array << word_letter
+			end
+		end
+		return new_array.join
+	end
 end
+
+
+
+
+
+
+
+
--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -1,6 +1,9 @@
 #my word game should...
 #take in a word on initalization
-#
+# letter_guess method should be a loop that allows the user to 
+# input a letter.
+# guesses are limited to the length of the word
+# it returns the matched letter 
 # RSpec
 # 'describe' block for each group of tests  
   # 'it' block for each individual test
@@ -8,13 +11,27 @@
 
 require_relative 'word_game.rb'
 
-describe WordGame do
-  let(:word) { WordGame.new("book") }
+describe WordGame do 
+	let(:word) { WordGame.new("book") }
 
-  it "stores a word given on initalization" do 
-  	  expect(word.new_word).to eq("book")
-  end
+	it "stores a word given on initalization" do 
+		expect(word.new_word).to eq("book")
+	end
+
+	it "allows the user to guess a letter" do 
+		expect(word.letter_guess("o")).to eq("_oo_")
+	end
+	
 end
+
+
+
+
+
+
+
+
+
 
 
 
