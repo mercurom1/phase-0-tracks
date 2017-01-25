1e2ef1b54bf1b0f617c95b0cc29c2fea7c82e87f
Michael Mercuro <mercurom1@montclair.edu>
2017-01-25 12:23:34 -0500

    Add comments and function

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+// program reverses a word that is set in as its parameter.
+// two variables, first var is the string given in
+// second var is the new empty string that is going to take in the reverse letters
+// output is the second var 
+// use a 'for' loop to decrement thru the string
+// use 'str.length - 1' to set 'i' as the last letter of the string 
+// add a conditional, and have 'i' add the next backwards letter to the string
+// by 'i--'
+// add colon marks at the end of each line or block of code
+
+function reverse(str) {
+	var reverseString = "";
+	for (var i = str.length -1; i >= 0; i--) {
+		reverseString += str[i];
+	}
+	return reverseString;
+} 
+
+
+//driver code
+console.log(reverse('mike'));
