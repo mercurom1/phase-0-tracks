14d109c5712352d5f43b75daf08ea80a05170d72
Michael Mercuro <mercurom1@montclair.edu>
2017-02-12 15:57:51 -0500

    Final commit

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,6 +7,19 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+campus_db = SQLite3::Database.new("campus.db")
+campus_db.results_as_hash = true
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS campus(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  )
+SQL
+
+campus_db.execute(create_table_cmd) 
+
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -23,5 +36,18 @@ post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+get "/campuses/new" do
+	erb :campus
+end
+
+post "/campuses" do 
+	campus_db.execute("INSERT INTO campus (name) VALUE (?)", [params['name']])
+	redirect '/'
+end
+
+
+
+
 
 # add static resources
